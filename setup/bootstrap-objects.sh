d3913e068d4bc4f3d40a5c1c22e5dd1efac6f453
Matt White <matt@matt-white.co.uk>
2021-03-21 21:13:02 +0000

    chore(bootstrap/objects): add crd provisioning

--- setup/bootstrap-objects.sh
+++ setup/bootstrap-objects.sh
@@ -31,9 +31,16 @@ installManualObjects(){
   kubectl -n kube-system create secret generic kms-vault --from-literal=account.json="$(echo $VAULT_KMS_ACCOUNT_JSON | base64 --decode)"
   kubectl -n kube-system create secret generic hcloud-api-token --from-literal=token="$HC_API_TOKEN"
 
+  
+  ##########
+  # crds
+  ##########
+  kustomize build ../cert-manager/crds > /tmp/build.yaml
+  kustomize build ../monitoring/kube-prometheus-stack/crds >> /tmp/build.yaml
+  cat ../kube-system/vault-secrets-operator/crd.yaml >> /tmp/build.yaml
+  kubectl apply -f /tmp/build.yaml && rm -f /tmp/build.yaml
 }
 
-#export KUBECONFIG="$REPO_ROOT/setup/kubeconfig"
 installManualObjects
 
 message "all done!"
